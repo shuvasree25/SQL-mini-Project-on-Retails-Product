defc6e09cd2c6f40b9895d3049b87ae37c096c94
Shuvasree Ray <shuvasree.ray99@gmail.com>
2026-07-09 00:03:52 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS Retail
+-- CREATE TABLE
+CREATE TABLE Products(
+product_id INT PRIMARY KEY,
+	  product_name VARCHAR(50),
+	  category VARCHAR(30),
+	  brand VARCHAR(30),
+	  price DECIMAL(10, 2),
+	  stock_quantity INT,
+	  rating DECIMAL (2, 1),
+	  launch_date DATE
+);
